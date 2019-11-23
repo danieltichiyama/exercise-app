5c0ca0e21544376fcc7afce1385c8db0536f692c
Briii <brianwkhuffman@gmail.com>
2019-11-23 09:22:24 -1000

    changes exercise_difficulty name to difficulties

--- server/database/migrations/20191122193428_exercise_difficulty.js
+++ server/database/migrations/20191122193428_exercise_difficulty.js
@@ -1,6 +1,6 @@
 
 exports.up = function (knex) {
-    return knex.schema.createTable("exercise_difficulty", table => {
+    return knex.schema.createTable("exercise_difficulties", table => {
         table.increments();
         table.string("exercise_difficulty");
         table.timestamps(true, true);
@@ -8,5 +8,5 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTable("exercise_difficulty");
+    return knex.schema.dropTable("exercise_difficulties");
 };
