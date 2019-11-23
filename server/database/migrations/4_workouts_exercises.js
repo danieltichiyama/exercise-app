c08bc399ab2d414da0c75f8792e422d69ef07937
Briii <brianwkhuffman@gmail.com>
2019-11-23 09:52:26 -1000

    adds exercises_bodyparts relation table, increments added to relation tables

--- server/database/migrations/4_workouts_exercises.js
+++ server/database/migrations/4_workouts_exercises.js
@@ -1,6 +1,7 @@
 
 exports.up = function (knex) {
     return knex.schema.createTable("workouts_exercises", table => {
+        table.increments();
         //foreign keys
         table.integer("workout_id").references("id").inTable("workouts");
         table.integer("exercise_id").references("id").inTable("exercises");
