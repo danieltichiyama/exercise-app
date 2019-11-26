2d254b7117b882420f55af2d08f9ee440de2dbe2
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-11-25 21:25:20 -1000

    adds a component_playground div to App.js and a button to show or hide it

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import SmokeButton from "../components/SmokeButton";
 
-function App() {
-  return (
-    <>
-      <SmokeButton></SmokeButton>
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playground: true
+    };
+  }
+
+  handleHidePlayground = () => {
+    let toggle = this.state.playground;
+    return this.setState({ playground: !toggle });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleHidePlayground}>Hide Playground</button>
+        {this.state.playground ? (
+          <div className="component_playground">
+            <SmokeButton></SmokeButton>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default App;
