8ecbd0b200c396996f263825fd1fde2a0f533c65
Janelle K Hirano <jaahnelle@gmail.com>
2019-11-30 12:15:29 -1000

    add toggle to mapDispatchToProps

---
+++ src/components/NavigationComponent/NavigationComponent.jsx
@@ -0,0 +1,96 @@
+import React { Component } from 'react';
+import { connect } from 'react-redux';
+import { actionsToggle } from '../../actions';
+
+class NavigationComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    // Home
+    handleHomeClick = () => {
+        this.props.toggle("home")
+    }
+
+    // Food
+    handleFoodClick = () => {
+        this.props.toggle("food")
+    }
+
+    // Fitness
+    handleFitnessClick = () => {
+        this.props.toggle("fitness")
+    }
+
+    // Community
+    handleCommunityClick = () => {
+        this.props.toggle("community")
+    }
+
+    // Profile
+    handleProfileClick = () => {
+        this.props.toggle("profile");
+    }
+
+    render() {
+        return (
+            <nav className="navigation">
+
+                {/* home */}
+                <img
+                    className="navButton"
+                    src="https://image.flaticon.com/icons/svg/846/846449.svg"
+                    alt="home button"
+                    onClick={this.handleHomeClick} />
+
+                {/* food */}
+                <img
+                    className="navButton"
+                    src="https://image.flaticon.com/icons/svg/1536/1536211.svg"
+                    alt="food button"
+                    onClick={this.handleFoodClick} />
+
+                {/* fitness */}
+                <img
+                    className="navButton"
+                    src="https://image.flaticon.com/icons/svg/1025/1025929.svg"
+                    alt="fitness button"
+                    onClick={this.handleFitnessClick} />
+
+                {/* community */}
+                <img
+                    className="navButton"
+                    src="https://image.flaticon.com/icons/svg/1159/1159603.svg"
+                    alt="community button"
+                    onClick={this.handleCommunityClick} />
+
+                {/* profile */}
+                <img
+                    className="navButton"
+                    src="https://image.flaticon.com/icons/svg/222/222299.svg"
+                    alt="profile button"
+                    onClick={this.handleProfileClick} />
+            </nav>
+        )
+    }
+}
+
+const mapStateToProps = stores => {
+    return {}
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        toggle: link => { 
+            return dispatch(actionsToggle(link));
+        }
+    }
+}
+
+NavigationComponent = connect(
+    mapStateToProps,
+    mapDispatchToProps)
+    (NavigationComponent);
+
+export default NavigationComponent;
