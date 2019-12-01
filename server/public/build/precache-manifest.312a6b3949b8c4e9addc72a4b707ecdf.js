921cb35f980b2d9fb8993f88186f8d815cb06b72
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-11-30 14:15:40 -1000

    devops

---
+++ server/public/build/precache-manifest.312a6b3949b8c4e9addc72a4b707ecdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28bd5feb759a56c21e94cea4549b5d7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a5e9594c9dc64b2c871",
+    "url": "/static/css/main.f7f79fba.chunk.css"
+  },
+  {
+    "revision": "b6780bc5d53fc6786615",
+    "url": "/static/js/2.557af241.chunk.js"
+  },
+  {
+    "revision": "5a5e9594c9dc64b2c871",
+    "url": "/static/js/main.390d195f.chunk.js"
+  },
+  {
+    "revision": "0d166061eca92059d50f",
+    "url": "/static/js/runtime-main.ed70e380.js"
+  }
+]);
