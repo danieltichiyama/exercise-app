dde16f9c798a59b78f77ac5b5b29124e663b06d2
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-12-08 11:40:14 -1000

    removes some console logs from reducer and from actions

--- server/api/nutrition/nutrition/index.js
+++ server/api/nutrition/nutrition/index.js
@@ -1,63 +1,68 @@
-const express = require('express');
+const express = require("express");
 const axios = require("axios");
 const nutritionRouter = express.Router();
 let fcdId;
 const foodSearchEndpoint = `https://api.nal.usda.gov/fdc/v1/search?api_key=${process.env.API_KEY}`;
 
-
-nutritionRouter.route("/")
+nutritionRouter
+  .route("/")
   .get((req, res) => {
     return axios({
-      method: 'post',
+      method: "post",
       url: foodSearchEndpoint,
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
       data: {
-        "generalSearchInput": req.body.data,
-        "includeDataTypes": {
-          "Survey (FNDDS)": true,
+        generalSearchInput: req.body.data,
+        includeDataTypes: {
+          "Survey (FNDDS)": true
         }
       }
-    }).then((response) => {
-      return res.json(response.data)
-    }).catch(err => {
-      console.log(err);
     })
+      .then(response => {
+        return res.json(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   })
   .post((req, res) => {
     return axios({
-      method: 'post',
+      method: "post",
       url: foodSearchEndpoint,
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
       data: {
-        "generalSearchInput": req.body.data,
-        "includeDataTypes": {
-          "Survey (FNDDS)": true,
+        generalSearchInput: req.body.data,
+        includeDataTypes: {
+          "Survey (FNDDS)": true
         }
       }
-    }).then((response) => {
-      return res.json(response.data);
-    }).catch(err => {
-      console.log(err);
     })
+      .then(response => {
+        return res.json(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   });
 
-nutritionRouter.route("/:id")
-  .get((req, res) => {
-    return axios({
-      method: 'get',
-      url: `https://api.nal.usda.gov/fdc/v1/${req.params.id}?api_key=${process.env.API_KEY}`,
-      headers: {
-        "Content-Type": "application/json",
-      }
-    }).then((response) => {
-      return res.json(response.data)
-    }).catch(err => {
-      console.log(err);
-    })
+nutritionRouter.route("/:id").get((req, res) => {
+  return axios({
+    method: "get",
+    url: `https://api.nal.usda.gov/fdc/v1/${req.params.id}?api_key=${process.env.API_KEY}`,
+    headers: {
+      "Content-Type": "application/json"
+    }
   })
+    .then(response => {
+      return res.json(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-module.exports = nutritionRouter
+module.exports = nutritionRouter;
