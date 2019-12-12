ad86c58f728f7389dfe4f9c8cd335488c80dc482
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-12-11 18:44:30 -1000

    starting user profile

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
 class UserPage extends Component {
+  constructor(props) {
+    super(props);
+
+    const getId = localStorage.getItem("session");
+    const id = JSON.parse(getId);
+  }
   render() {
     return <div>User Page</div>;
   }
