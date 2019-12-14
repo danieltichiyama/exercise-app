01a29ac2d4b9c2d7abaaf73faf80d0c695f9663e
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-12-14 10:49:35 -1000

    remove ignored files

--- src/pages/UserEditPage/UserEditPage.jsx
+++ src/pages/UserEditPage/UserEditPage.jsx
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { actionLoadUser } from "../../actions";
 
 class UserEditPage extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+
+    let getId = localStorage.getItem("session");
+    let id = JSON.parse(getId);
+
+    this.state = {
+      id,
+      name: "",
+      birth_date: "00/00/0000"
+    };
+  }
+
+  handleSubmit() {}
+
   render() {
     return (
-      <div>
-        EDIT MY SHIZ
-        <button>
-          <Link to="/user">Cancel</Link>
-        </button>
-      </div>
+      <>
+        <form onSubmit={this.handleSubmit}></form>
+        <div>
+          EDIT MY SHIZ
+          <button>
+            <Link to="/user">Cancel</Link>
+          </button>
+        </div>
+      </>
     );
   }
 }
