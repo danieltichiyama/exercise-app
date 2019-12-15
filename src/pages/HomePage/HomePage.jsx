738c8cbcb8178310a24f420afe084bf8f33a9a88
Janelle K Hirano <jaahnelle@gmail.com>
2019-12-14 17:05:30 -1000

    add header

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 
 import DashboardComponent from "../../components/DashboardComponent";
 import NewsFeedComponent from "../../components/NewsFeedComponent";
+import styles from "../HomePage/HomePage.module.scss";
 
 class HomePage extends Component {
   render() {
     return (
       <>
+        <div className={styles.homePageHeader}>
+          <span className={styles.fit}>fit</span>
+          <span className={styles.works}>works</span>
+        </div>
         <DashboardComponent></DashboardComponent>
         <NewsFeedComponent></NewsFeedComponent>
       </>
