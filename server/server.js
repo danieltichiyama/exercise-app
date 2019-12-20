db16c9f805529912fdf479be908f15c98a7ff317
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-12-20 03:16:04 -1000

    updates the version of workbox to import in custom-sw.js and adds a .registerNavigationRoute to catch all urls typed into the address bar

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ app.get("/smoke", (req, res) => {
 //necessary for local spin up of front end with npm run build, remove before deploying
 //use localhost:8080/ for npm run build
 app.get(`*`, function(req, res) {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
+  res.sendFile(path.join(__dirname, "../build", "sw.js"));
 });
 
 app.listen(PORT, () => {
--- src/sw-custom.js
+++ src/sw-custom.js
@@ -1,12 +1,12 @@
 if ("function" === typeof importScripts) {
   importScripts(
-    "https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js"
+    "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
   );
   // Global workbox
   if (workbox) {
     console.log("Workbox is loaded");
     // Disable logging
-    // workbox.setConfig({ debug: false });
+    workbox.setConfig({ debug: false });
     //`generateSW` and `generateSWString` provide the option
     // to force update an exiting service worker.
     // Since we're using `injectManifest` to build SW,
@@ -17,7 +17,9 @@ if ("function" === typeof importScripts) {
     });
     // Manual injection point for manifest files.
     // All assets under build/ and 5MB sizes are precached.
+
     workbox.precaching.precacheAndRoute([]);
+
     //precacheAndRoute creates an instance of PreCacheController under the hood and also creates it's own fetch eventlisteners for these routes (this the AndRoute bit);
 
     // Font caching
@@ -32,6 +34,22 @@ if ("function" === typeof importScripts) {
         ]
       })
     );
+
+    console.log("precaching", workbox.precaching);
+    // //catch all and send back to index.html
+    workbox.routing.registerNavigationRoute(
+      workbox.precaching.getCacheKeyForURL("/index.html"),
+      //not working...
+      {
+        blacklist: [
+          new RegExp("^/api/"), // Exclude URLs starting with /api/, as they're API calls (not navigation)
+          new RegExp("/[^/]+\\.[^/]+$") // Exclude URLs containing a dot, as they're likely a resource in public/ and not a SPA route
+        ]
+      }
+    );
+
+    //next try to setup a registerNR for error handling and going to pages that aren't found in the cached page...
+
     // Image caching
     workbox.routing.registerRoute(
       /\.(?:png|gif|jpg|jpeg|svg)$/,
@@ -65,7 +83,7 @@ if ("function" === typeof importScripts) {
 }
 
 //sets up background sync for GET and POST requests.
-const bgSyncPlugin = new workbox.backgroundSync.Plugin("myQueueName", {
+const bgSyncPlugin = new workbox.backgroundSync.Plugin("pending-requests", {
   maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
 });
 
@@ -82,4 +100,20 @@ workbox.routing.registerRoute(
     plugins: [bgSyncPlugin]
   }),
   "POST"
+);
+
+workbox.routing.registerRoute(
+  /\/api\/.*/,
+  new workbox.strategies.NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  "PUT"
+);
+
+workbox.routing.registerRoute(
+  /\/api\/.*/,
+  new workbox.strategies.NetworkOnly({
+    plugins: [bgSyncPlugin]
+  }),
+  "DELETE"
 );
