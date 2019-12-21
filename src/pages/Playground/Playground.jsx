f7122d77f822f5a91288ed82a0e2615a24471e30
Calvin <calmoon808@gmail.com>
2019-12-20 19:36:53 -1000

    reverted playground

--- src/pages/Playground/Playground.jsx
+++ src/pages/Playground/Playground.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import styles from "./Playground.module.scss";
-import FoodVisionComponent from "../../components/FoodVisionComponent"
-import FoodSearchComponent from "../../components/FoodSearchComponent/FoodSearchComponent";
 
 class Playground extends Component {
   constructor(props) {
@@ -13,8 +11,6 @@ class Playground extends Component {
       <div className={styles.Playground}>
         <p>No Pushing. No jumping. Have fun.</p>
         {/* insert playground equipment here. */}
-        <FoodVisionComponent/>
-        <FoodSearchComponent/>
       </div>
     );
   }
