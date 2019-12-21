33222b23de8323913cdf779cd1ceb179782164aa
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-12-20 23:17:24 -1000

    moves communitypostscomponent's css to module scope'

--- src/components/AddMealButtonComponent/AddMealButtonComponent.jsx
+++ src/components/AddMealButtonComponent/AddMealButtonComponent.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../AddMealButtonComponent/AddMealButtonComponent.module.scss";
 
@@ -7,41 +7,39 @@ import addLunch from "../../icons/addLunch.png";
 import addDinner from "../../icons/addDinner.png";
 import addSnack from "../../icons/addSnack.png";
 
-class AddMealButtonComponent extends Component {
-  render() {
-    return (
-      <div className={styles.mealNavigation}>
-        <div>
-          <Link to="/nutrition/add_breakfast">
-            <button className={styles.mealButton}>
-              <img src={addBreakfast} alt="breakfast" />
-            </button>
-          </Link>
-        </div>
-        <div>
-          <Link to="/nutrition/add_lunch">
-            <button className={styles.mealButton}>
-              <img src={addLunch} alt="lunch" />
-            </button>
-          </Link>
-        </div>
-        <div>
-          <Link to="/nutrition/add_dinner">
-            <button className={styles.mealButton} value="3">
-              <img src={addDinner} alt="dinner" />
-            </button>
-          </Link>
-        </div>
-        <div>
-          <Link to="/nutrition/add_snack">
-            <button className={styles.mealButton} value="4">
-              <img src={addSnack} alt="snack" />
-            </button>
-          </Link>
-        </div>
+const AddMealButtonComponent = props => {
+  return (
+    <div className={styles.mealNavigation}>
+      <div>
+        <Link to="/nutrition/add_breakfast">
+          <button className={styles.mealButton}>
+            <img src={addBreakfast} alt="breakfast" />
+          </button>
+        </Link>
       </div>
-    );
-  }
-}
+      <div>
+        <Link to="/nutrition/add_lunch">
+          <button className={styles.mealButton}>
+            <img src={addLunch} alt="lunch" />
+          </button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/nutrition/add_dinner">
+          <button className={styles.mealButton} value="3">
+            <img src={addDinner} alt="dinner" />
+          </button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/nutrition/add_snack">
+          <button className={styles.mealButton} value="4">
+            <img src={addSnack} alt="snack" />
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default AddMealButtonComponent;
