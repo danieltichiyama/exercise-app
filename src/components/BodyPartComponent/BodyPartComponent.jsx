16ddc61c484ee35b14149e085ec5844f4f5c7e1b
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-12-23 21:34:53 -1000

    save point

--- src/components/BodyPartComponent/BodyPartComponent.jsx
+++ src/components/BodyPartComponent/BodyPartComponent.jsx
@@ -1,76 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import styles from "./BodyPartComponent.module.scss";
 import { connect } from "react-redux";
-import { actionsLoadBodyParts, actionsFilterBodyParts, actionsLoadExerciseList } from '../../actions';
+import {
+  actionsLoadBodyParts,
+  actionsFilterBodyParts,
+  actionsLoadExerciseList
+} from "../../actions";
 
 class BodyPartComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            bodypartID: ''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      bodypartID: ""
+    };
+  }
 
-    componentDidMount() {
-        this.props.dispatchLoadBodyParts();
-    }
+  componentDidMount() {
+    this.props.dispatchLoadBodyParts();
+  }
 
-    handleBodyPartClick = (e) => {
-        e.preventDefault();
-        this.props.dispatchFilterBodyParts(e.target.id);
-    }
+  handleBodyPartClick = e => {
+    e.preventDefault();
+    this.props.dispatchFilterBodyParts(e.target.id);
+  };
 
-    handleShowAllBodyParts = (e) => {
-        e.preventDefault();
-        this.props.dispatchLoadExerciseList();
-    }
+  handleShowAllBodyParts = e => {
+    e.preventDefault();
+    this.props.dispatchLoadExerciseList();
+  };
 
-    render() {
-        return (
-            <>
-                < button
-                    onClick={this.handleShowAllBodyParts}
-                    className={styles.bodypartbutton}
-                >ALL</button >
-                {this.props.bodyparts.map(bodypart => {
-                    return (
-                        <div
-                            className={styles.bodypart}
-                            key={bodypart.id}
-                        >
-                            <button
-                                className={styles.bodypartbutton}
-                                id={bodypart.id}
-                                onClick={this.handleBodyPartClick}>{bodypart.bodypart}</button>
-                        </div>
-                    )
-                })}
-            </>
-        )
-    }
+  render() {
+    return (
+      <div className={styles.BodyParts}>
+        <button
+          onClick={this.handleShowAllBodyParts}
+          className={styles.bodypartbutton}
+        >
+          Show all
+        </button>
+        {this.props.bodyparts.map(bodypart => {
+          return (
+            <button
+              className={styles.bodypartbutton}
+              id={bodypart.id}
+              onClick={this.handleBodyPartClick}
+              key={bodypart.id}
+            >
+              {bodypart.bodypart}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = store => {
-    return {
-        bodyparts: store.bodyparts,
-        exercise: store.exercise
-    }
-}
+  return {
+    bodyparts: store.bodyparts,
+    exercise: store.exercise
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-        dispatchLoadBodyParts: () => {
-            return dispatch(actionsLoadBodyParts());
-        },
-        dispatchFilterBodyParts: (data) => {
-            return dispatch(actionsFilterBodyParts(data));
-        },
-        dispatchLoadExerciseList: () => {
-            return dispatch(actionsLoadExerciseList());
-        }
+  return {
+    dispatchLoadBodyParts: () => {
+      return dispatch(actionsLoadBodyParts());
+    },
+    dispatchFilterBodyParts: data => {
+      return dispatch(actionsFilterBodyParts(data));
+    },
+    dispatchLoadExerciseList: () => {
+      return dispatch(actionsLoadExerciseList());
     }
-}
+  };
+};
 
-BodyPartComponent = connect(mapStateToProps, mapDispatchToProps)(BodyPartComponent);
+BodyPartComponent = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BodyPartComponent);
 
 export default BodyPartComponent;
--- src/pages/ExercisePage/ExercisePage.jsx
+++ src/pages/ExercisePage/ExercisePage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styles from "./ExercisePage.module.scss"
+import styles from "./ExercisePage.module.scss";
 import BodyPartComponent from "../../components/BodyPartComponent/BodyPartComponent";
 import ExerciseListComponent from "../../components/ExerciseListComponent/ExerciseListComponent";
 import { Link } from "react-router-dom";
@@ -7,19 +7,20 @@ import { Link } from "react-router-dom";
 class ExercisePage extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
   render() {
     return (
-      <div>
-        <h1 className={styles.header}>EXERCISE LIST</h1>
-        <Link to="/workout" style={{ textDecoration: 'none' }}>
+      <div className={styles.ExercisePage}>
+        <input className={styles.exerciseSearchPH} type="text" />
+        <Link to="/workout" style={{ textDecoration: "none" }}>
           <h2>Workout Log</h2>
         </Link>
+
         <BodyPartComponent />
         <ExerciseListComponent />
       </div>
-    )
+    );
   }
 }
 
