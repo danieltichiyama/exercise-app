b28f9d77578a4f6200f3b09260017d875adb7981
Janelle K Hirano <jaahnelle@gmail.com>
2019-12-23 21:48:52 -1000

    add css to addFoodPopUpComponent, add modal to  AddMealButtonComponent

--- src/components/AddMealButtonComponent/AddMealButtonComponent.jsx
+++ src/components/AddMealButtonComponent/AddMealButtonComponent.jsx
@@ -1,45 +1,84 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styles from "../AddMealButtonComponent/AddMealButtonComponent.module.scss";
+import AddFoodPopUpComponent from "../AddFoodPopUpComponent";
 
 import addBreakfast from "../../imgs/addBreakfast.png";
 import addLunch from "../../imgs/addLunch.png";
 import addDinner from "../../imgs/addDinner.png";
 import addSnack from "../../imgs/addSnack.png";
 
-const AddMealButtonComponent = props => {
-  return (
-    <div className={styles.mealNavigation}>
-      <div>
-        <Link to="/nutrition/add_breakfast">
-          <button className={styles.mealButton}>
-            <img src={addBreakfast} alt="breakfast" />
+class AddMealButtonComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPopUp: false,
+      meal_type: 0
+    };
+  }
+
+  handleFoodPopUp = e => {
+    console.log("e.target", e.target);
+    let { id } = e.target;
+
+    console.log("id:", id);
+    // let { meal_type } = e.target.dataset;
+
+    this.setState({
+      meal_type: id,
+      showPopUp: !this.state.showPopUp
+    });
+  };
+
+  render() {
+    return (
+      <div className={styles.mealNavigation}>
+        {this.state.showPopUp ? (
+          <AddFoodPopUpComponent meal_type={this.state.meal_type} />
+        ) : null}
+
+        <div>
+          <button
+            className={styles.mealButton}
+            id="1"
+            onClick={this.handleFoodPopUp}
+          >
+            <img src={addBreakfast} id="1" alt="breakfast" />
           </button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/nutrition/add_lunch">
-          <button className={styles.mealButton}>
+        </div>
+
+        <div>
+          <button
+            className={styles.mealButton}
+            value="2"
+            onClick={this.handleFoodPopUp}
+          >
             <img src={addLunch} alt="lunch" />
           </button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/nutrition/add_dinner">
-          <button className={styles.mealButton} value="3">
+        </div>
+
+        <div>
+          <button
+            className={styles.mealButton}
+            value="3"
+            onClick={this.handleFoodPopUp}
+          >
             <img src={addDinner} alt="dinner" />
           </button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/nutrition/add_snack">
-          <button className={styles.mealButton} value="4">
+        </div>
+
+        <div>
+          <button
+            className={styles.mealButton}
+            value="4"
+            onClick={this.handleFoodPopUp}
+          >
             <img src={addSnack} alt="snack" />
           </button>
-        </Link>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default AddMealButtonComponent;
