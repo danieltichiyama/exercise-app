da62467cc8dcc38b3c1aa92ae8c3d391f704db27
Janelle K Hirano <jaahnelle@gmail.com>
2019-12-23 21:18:57 -1000

    fix no rows deleted error

--- server/api/nutrition/fat_secret/index.js
+++ server/api/nutrition/fat_secret/index.js
@@ -8,43 +8,41 @@ const FatSecret = require("../../../database/models/FatSecret");
 let timer;
 
 var options = {
-   method: 'POST',
-   url: 'https://oauth.fatsecret.com/connect/token',
-   auth : {
-      user : clientID,
-      password : clientPW
-   },
-   headers: { 'Content-Type': 'application/json'},
-   form: {
-      'grant_type': 'client_credentials',
-      'scope' : 'basic'
-   },
-   json: true
-  };
-  
-function setToken(){
+  method: "POST",
+  url: "https://oauth.fatsecret.com/connect/token",
+  auth: {
+    user: clientID,
+    password: clientPW
+  },
+  headers: { "Content-Type": "application/json" },
+  form: {
+    grant_type: "client_credentials",
+    scope: "basic"
+  },
+  json: true
+};
+
+function setToken() {
   let token;
   return {
-    refreshToken: function refreshToken(obj){
+    refreshToken: function refreshToken(obj) {
       token = obj;
-      return token
+      return token;
     },
-    getToken: function getToken(){
+    getToken: function getToken() {
       return token;
     }
-  }
-} 
+  };
+}
 
 let tokenClosure = setToken();
 
 let fatSecretApi = () => {
-  new FatSecret()
-  .fetchAll()
-  .then(async response => {
+  new FatSecret().fetchAll().then(async response => {
     //check if oauth token data table is empty
-    if (response.length === 0){
+    if (response.length === 0) {
       //if oauth token data table is empty get new token
-      request(options, function (error, response, body) {
+      request(options, function(error, response, body) {
         if (error) {
           throw new Error(error);
         } else {
@@ -53,77 +51,80 @@ let fatSecretApi = () => {
             Oauth2Token: body.access_token,
             expires_in: body.expires_in
           })
-          .save()
-          .then(() => {
-            //set timer to refresh token;
-            timer = setInterval(() => {
-              fatSecretApi();
-            }, 82,800,000)
-          });
-        };
+            .save()
+            .then(() => {
+              //set timer to refresh token;
+              timer = setInterval(
+                () => {
+                  fatSecretApi();
+                },
+                82,
+                800,
+                000
+              );
+            });
+        }
       });
     } else {
       //if oauthtoken table not empty delete it
       new FatSecret()
-      .where({
-        Oauth2Token: tokenClosure.getToken().access_token
-      })
-      .destroy()
-      //stop timer then re run function
-      .then(() => {
-        clearInterval(timer);
-        fatSecretApi();
-      })
+        .where({
+          Oauth2Token: tokenClosure.getToken().access_token
+        })
+        .destroy({ require: false })
+        //stop timer then re run function
+        .then(() => {
+          clearInterval(timer);
+          fatSecretApi();
+        });
     }
-  })
-}
+  });
+};
 
 fatSecretApi();
 
-fatSecretRouter.route("/")
-.post((req, res) => {
+fatSecretRouter.route("/").post((req, res) => {
   return axios({
-    method: 'post',
+    method: "post",
     url: "https://platform.fatsecret.com/rest/server.api",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${tokenClosure.getToken().access_token}`
+      Authorization: `Bearer ${tokenClosure.getToken().access_token}`
     },
     params: {
-      method : "foods.search",
+      method: "foods.search",
       search_expression: req.body.data,
       format: "json"
     }
   })
-  .then(response => {
-    return res.json(response.data);
-  })
-  .catch(err => {
-    console.log(err);
-  })
-})
+    .then(response => {
+      return res.json(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
-fatSecretRouter.route("/nutrients")
-.post((req, res) => {
+fatSecretRouter.route("/nutrients").post((req, res) => {
   return axios({
-    method: 'post',
+    method: "post",
     url: "https://platform.fatsecret.com/rest/server.api",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${tokenClosure.getToken().access_token}`
+      Authorization: `Bearer ${tokenClosure.getToken().access_token}`
     },
     params: {
-      method : "food.get",
+      method: "food.get",
       food_id: req.body.data,
       format: "json"
     }
   })
-  .then(response => {
-    return res.json(response.data);
-  })
-  .catch(err => {
-    console.log(err);
-  })
-})
+    .then(response => {
+      return res.json(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 module.exports = fatSecretRouter;
