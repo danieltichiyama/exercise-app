ecb03aef84b51d52730e365f73806241fdd1607d
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-12-25 19:34:36 -1000

    adds video player to playground with responsive sizing, does not work in exercise list page for some reason

--- src/pages/Playground/Playground.jsx
+++ src/pages/Playground/Playground.jsx
@@ -1,27 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./Playground.module.scss";
-import SmokeButton from "../../components/SmokeButton";
-import FoodVisionComponent from "../../components/FoodVisionComponent";
-import FatSecretSearchComponent from "../../components/FatSecretSearchComponent";
-import ImageUploadComponent from "../../components/ImageUploadComponent";
+import VideoPlayerComponent from "../../components/VideoPlayerComponent";
 
-class Playground extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className={styles.Playground}>
-        <p>Pushing and pulling encouraged. Have fun.</p>
-        {/* insert playground equipment here. */}
-        <FoodVisionComponent></FoodVisionComponent>
-        <FatSecretSearchComponent></FatSecretSearchComponent>
-        <ImageUploadComponent></ImageUploadComponent>
-        <SmokeButton></SmokeButton>
-      </div>
-    );
-  }
-}
+const Playground = props => {
+  return (
+    <div className={styles.Playground}>
+      <p>Pushing and pulling encouraged. Have fun.</p>
+      {/* insert playground equipment here. */}
+      <VideoPlayerComponent></VideoPlayerComponent>
+    </div>
+  );
+};
 
 export default Playground;
